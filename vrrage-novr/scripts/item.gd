c6c3fb7933b5fba85e3c43c90ca22e7529290a9f
UltimateExergon <44118434+UltimateExergon@users.noreply.github.com>
2025-01-17 17:30:08 +0100

    Updated destruction.gd, main.gd, item.gd

--- vrrage-novr/scripts/item.gd
+++ vrrage-novr/scripts/item.gd
@@ -1,7 +1,20 @@
 extends RigidBody3D
 
+@export var dropID : int : set = set_dropID, get = get_dropID
+
 func _ready():
+	self.contact_monitor = true
+	self.max_contacts_reported = 160
+	self.add_to_group("CRAFTABLE")
 	self.body_entered.connect(_on_body_entered)
 	
-func _on_body_entered():
-	pass
+func _on_body_entered(body):
+	if body.is_in_group("CRAFTABLE"):
+		print("Crafting:", self, body)
+		get_tree().root.get_children()[0].craft(self, body)
+		
+func set_dropID(id : int) -> void:
+	dropID = id
+	
+func get_dropID() -> int:
+	return dropID
--- vrrage-novr/scripts/main.gd
+++ vrrage-novr/scripts/main.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
 const levelPath : String = "res://scenes/Level/"
+const itemPath : String = "res://scenes/items/"
 const recipePath : String = "res://craftingRecipes/"
 const levelFormat : String = ".tscn"
+const itemFormat : String = ".tscn"
 const recipeFormat : String = ".csv"
 
+var current_level : String
 var startPos : Vector3
 var craftingRecipes : Array
+var ingredients : Array
 
 func _ready():
 	load_level("testlevel")
@@ -21,6 +25,7 @@ func load_level(levelname : String) -> void:
 	delete_oldLevel()
 	
 	print("Loading " + levelname)
+	current_level = levelname
 	craftingRecipes = load(recipePath + levelname + recipeFormat).records
 	var level = load(levelPath + levelname + levelFormat).instantiate()
 	add_child(level)
@@ -32,6 +37,37 @@ func load_level(levelname : String) -> void:
 	
 func load_player():
 	var player = load("res://scenes/player.tscn").instantiate()
+	player.global_position = startPos
 	add_child(player)
-	player.position = startPos
+	
+func craft(item1, item2):
+	ingredients.append([item1, item2])
+	if ingredients.size() == 2:
+		var new_item = match_items()
+		if new_item != null:
+			spawn_crafted_item(new_item, item1.global_position)
+			item1.queue_free()
+			item2.queue_free()
+			
+func spawn_crafted_item(itemID : int, pos : Vector3):
+	var item = load(itemPath + current_level + "/" + str(itemID) + itemFormat).instantiate()
+	item.position = pos
+	get_node(current_level).add_child(item)
+		
+func check_for_recipe(items : Array):
+	for i in craftingRecipes:
+		var recipe_ingredients : Array = [i[0], i[1]]
+		if items[0].get_dropID() in recipe_ingredients and items[1].get_dropID() in recipe_ingredients:
+			return i[2]
+			
+	return null
+		
+func match_items():
+	var item1 : Array = ingredients[0]
+	var item2 : Array = ingredients[1]
+	
+	if item1[0] == item2[1] and item1[1] == item2[0]:
+		return check_for_recipe(item1)
+	else:
+		return null
 	
