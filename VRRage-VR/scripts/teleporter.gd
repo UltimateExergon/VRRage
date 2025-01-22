0b3595a9895036c7dc745188a882dc4d81c0e490
UltimateExergon <44118434+UltimateExergon@users.noreply.github.com>
2025-01-22 15:25:32 +0100

    Added teleporters and birthday level

---
+++ VRRage-VR/scripts/teleporter.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var teleport_level : String
+@export var label_text : String
+
+@onready var main_node := get_tree().root.get_children()[2]
+
+func _ready():
+	$Label3D.text = "To: " + label_text
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("PLAYER"):
+		main_node.load_level(teleport_level)
