0d333d62583b2a4cdf6efe871ba140c917e72eea
Julian Wolf <der_julian@outlook.de>
2025-02-06 18:47:14 +0100

    Fixed office wall highlight bug

--- VRRage-VR/scripts/player.gd
+++ VRRage-VR/scripts/player.gd
@@ -3,11 +3,11 @@ extends XROrigin3D
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("CRAFTABLE"):
 		body.add_outline_color(Globals.outline_color_crafting_near)
-	elif body is XRToolsPickable:
+	elif body is XRToolsPickable and body.enabled == true:
 		body.add_outline_color(Globals.outline_color_near)
 
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("CRAFTABLE"):
 		body.change_highlight(Globals.outline_color_crafting)
-	elif body is XRToolsPickable:
+	elif body is XRToolsPickable and body.enabled == true:
 		body.add_outline_color(Globals.outline_color_near)
