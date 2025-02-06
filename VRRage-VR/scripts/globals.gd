731c9dfad73bc2f0a96a8b374a6b4052221e4532
Arthur P. <35407735+arqtos@users.noreply.github.com>
2025-02-06 18:00:37 +0100

    adjust highlight color

--- VRRage-VR/scripts/globals.gd
+++ VRRage-VR/scripts/globals.gd
@@ -13,6 +13,6 @@ const recipeFormat : String = ".csv"
 @onready var destructionScore = preload("res://scenes/destruction_score.tscn")
 
 @onready var outline_shader = preload("res://shader/outline.gdshader")
-const outline_color : Color = Color8(255, 255, 255, 255)
+const outline_color : Color = Color8(255, 255, 255, 50)
 const outline_color_near : Color = Color8(91, 255, 92, 255)
 const outline_width : float = 2.0
