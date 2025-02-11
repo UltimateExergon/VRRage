245a39f3c1a08ba1b13e7172aad142b144bd919e
Julian Wolf <der_julian@outlook.de>
2025-02-11 22:03:28 +0100

    Changed CollisionLayer Names

--- VRRage-VR/scripts/level.gd
+++ VRRage-VR/scripts/level.gd
@@ -4,6 +4,11 @@ extends Node3D
 
 func _ready():
 	print("Loaded level: ", self.name)
+	
+	for i in get_children():
+		if i.is_in_group("room"):
+			i.set_collision_mask(0, false)
+			i.set_collision_layer(0, true)
 
 func get_startPos() -> Vector3:
 	return start_pos
