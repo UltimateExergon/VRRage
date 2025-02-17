34a2c565b78f27c58b7d18b68c8aab23d10a3576
Julian Wolf <der_julian@outlook.de>
2025-02-17 18:29:28 +0100

    New Office Models, score multipliers

---
+++ VRRage-VR/scripts/office/coffe_machine.gd
@@ -0,0 +1,9 @@
+extends "res://scripts/item.gd"
+
+@export var score_multiplier : float = 2.0
+@export var multiplier_time : float = 120.0
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("MUG"):
+		body.queue_free()
+		main_node.set_timed_multiplier(score_multiplier, multiplier_time)
